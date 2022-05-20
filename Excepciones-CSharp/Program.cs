5c77e841d07b00aa36d36657c0a0d063e73e0d68
ObedVPSM256 <105815117+ObedVPSM256@users.noreply.github.com>
2022-05-20 11:52:38 -0700

    Add files via upload

---
+++ Excepciones-CSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Introduzca un número: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Introduzca otro número: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.Write(e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+    }
+}
